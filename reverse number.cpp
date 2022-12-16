edd67dc70b3c97de9910f6e64e42d731508bc74b
0varun <120158648+0varun@users.noreply.github.com>
2022-12-16 10:08:57 +0530

    Create reverse number.cpp

---
+++ reverse number.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using name space std;
+int main(){
+	int n;
+	cin>>n;
+	int(reminder);
+	int(reverse);
+	while(n>0){
+		reminder=number%10;
+		reverse=reverse*10+reminder;
+		num/=10
+	}
+	print("enter a number");
+} 
